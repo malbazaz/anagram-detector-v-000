b95f823990fb9b6757fde2723c319d0632eab430
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-20 20:53:10 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ end
 def match(array)
  array.each do |actual|
 actual_array = actual.split("")
-actual_array.sort == @word_array.sort
+if actual_array.sort == @word_array.sort
         true 
         else 
         false 
