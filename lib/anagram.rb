709d1f0636a9eb193e0368b74b5697b42a169769
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-20 20:41:27 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,12 +6,11 @@ attr_accessor :word
 def initialize(word)
   @word = word
  @word_array = word.split("")
-  @sorted_word_array = word_array.sort  
 end 
 
 def match(array)
  array.each do |actual|
-      if actual.sort == sorted_word_array
+      if actual.sort == word_array.sort
         true 
         else 
         false 
