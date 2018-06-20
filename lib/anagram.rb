17518ac5e51f65bb1269e0626b40f644c7ac133b
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-20 20:34:28 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,9 +8,14 @@ def initialize(word)
 end 
 
 def match(array)
-new_word = word.
-if array.include?()
-
+  word_array = word.split("")
+  sorted_word_array = word_array.sort
+    array.each do |actual|
+      if actual.sort == sorted_word_array
+        true 
+        else 
+        false 
+      end 
 end 
-
+end 
 end 
